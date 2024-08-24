47b50a6caea3ae0c055bd54f55e280ec1add0e27
mizelleaustin <mizelleaustin@gmail.com>
2024-08-23 20:02:23 -0500

    Week 1 Programming Files

---
+++ Week 1/multiply.py
@@ -0,0 +1,11 @@
+# Simple Python program to multiply two numbers
+
+# Define two numbers
+number1 = 6
+number2 = 7
+
+# Multiply the numbers
+result = number1 * number2
+
+# Print the result
+print("The result of multiplying", number1, "and", number2, "is:", result)
